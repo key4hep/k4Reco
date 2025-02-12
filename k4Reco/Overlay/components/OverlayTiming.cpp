b31e098cd171c367551b6599865d92f4f53afc0f
jmcarcell <jmcarcell@users.noreply.github.com>
2025-02-12 11:03:00 +0100

    Fix setting the time for tracker hits

--- k4Reco/Overlay/components/OverlayTiming.cpp
+++ k4Reco/Overlay/components/OverlayTiming.cpp
@@ -302,19 +302,17 @@ retType OverlayTiming::operator()(const edm4hep::EventHeaderCollection&         
             continue;
           }
           auto& ocoll = osimTrackerHits[i];
-          if (std::abs(timeOffset) < std::numeric_limits<float>::epsilon()) {
-            for (const auto&& simTrackerHit : backgroundEvent.get<edm4hep::SimTrackerHitCollection>(name)) {
-              const float tof = time_of_flight(simTrackerHit.getPosition());
+          for (const auto&& simTrackerHit : backgroundEvent.get<edm4hep::SimTrackerHitCollection>(name)) {
+            const float tof = time_of_flight(simTrackerHit.getPosition());
 
-              if (!((simTrackerHit.getTime() + timeOffset > this_start + tof) &&
-                    (simTrackerHit.getTime() + timeOffset < this_stop + tof)))
-                continue;
-              auto nhit = simTrackerHit.clone(false);
-              nhit.setOverlay(true);
-              nhit.setTime(simTrackerHit.getTime() + timeOffset);
-              nhit.setParticle(oparticles[oldToNewMap[simTrackerHit.getParticle().getObjectID().index]]);
-              ocoll->push_back(nhit);
-            }
+            if (!((simTrackerHit.getTime() + timeOffset > this_start + tof) &&
+                  (simTrackerHit.getTime() + timeOffset < this_stop + tof)))
+              continue;
+            auto nhit = simTrackerHit.clone(false);
+            nhit.setOverlay(true);
+            nhit.setTime(simTrackerHit.getTime() + timeOffset);
+            nhit.setParticle(oparticles[oldToNewMap[simTrackerHit.getParticle().getObjectID().index]]);
+            ocoll->push_back(nhit);
           }
         }
 
