ece5bbcf2105efaf9ccae81007313d6f020b0dc5
jmcarcell <jmcarcell@users.noreply.github.com>
2025-03-12 11:06:18 +0100

    Fix pre-commit and warnings

--- k4Reco/ConformalTracking/include/KDTrack.h
+++ k4Reco/ConformalTracking/include/KDTrack.h
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2020-2024 Key4hep-Project.
+ *
+ * This file is part of Key4hep.
+ * See https://key4hep.github.io/key4hep-doc/ for further info.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #ifndef K4RECO_KDTRACK_H
 #define K4RECO_KDTRACK_H
 
@@ -30,15 +48,9 @@ public:
   ~KDTrack()                         = default;
 
   //--- Functions to add and remove clusters
-  void add(SKDCluster cluster) {
-    m_clusters.push_back(cluster);
-  }
-  void insert(SKDCluster cluster) {
-    m_clusters.insert(m_clusters.begin(), cluster);
-  }
-  void remove(int clusterN) {
-    m_clusters.erase(m_clusters.begin() + clusterN);
-  }
+  void add(SKDCluster cluster) { m_clusters.push_back(cluster); }
+  void insert(SKDCluster cluster) { m_clusters.insert(m_clusters.begin(), cluster); }
+  void remove(int clusterN) { m_clusters.erase(m_clusters.begin() + clusterN); }
 
   //--- Fit functions
   double calculateChi2();
@@ -91,7 +103,7 @@ public:
   bool   fillFit       = false;
 
   // Clusters and kalman track pointer
-  double           m_pT      = 0.0;
+  double           m_pT = 0.0;
   SharedKDClusters m_clusters{};
   bool             m_kalmanFitForward = true;
 };
