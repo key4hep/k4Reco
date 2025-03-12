556fa7e906bf433945f20deee7ff32db0d67c6dc
jmcarcell <jmcarcell@users.noreply.github.com>
2025-03-12 10:11:57 +0100

    Add ConformalTracking, ported from LCSoft

---
+++ k4Reco/ConformalTracking/include/KDTree.h
@@ -0,0 +1,81 @@
+#ifndef K4RECO_KDTREE_H
+#define K4RECO_KDTREE_H
+
+#include "KDCluster.h"
+#include "kdtree2.h"
+
+#include <boost/array.hpp>
+#include <boost/multi_array.hpp>
+
+#include <memory>
+#include <vector>
+#include <map>
+
+//-----------------------------------------------------------------------------
+// Class : KDTree
+//
+// Info : Wrapper interface to the kdtree2 class, to be passed a vector of
+// clusters on a given plane. Will return the kNN to a given seed point.
+//
+// 2011-07-21 : Matthew Reid
+//
+//-----------------------------------------------------------------------------
+
+// How to use:-
+// initialise KDTree nn(<VecCluster> plane_i);
+// VecCluster result(N);
+// nn2.nearestNeighbours(<TestBeamCluster> seed, N, result);
+// Where N is the number of nearest neighbours to be returned
+
+// Define some typedefs for use later
+typedef std::vector<SKDCluster> VecCluster;
+
+// build the class
+class KDTree {
+public:
+  typedef kdtree2::KDTreeResultVector KDTreeResultVector;
+
+  explicit KDTree(const SharedKDClusters& pts, double overlapTheta, bool sort);
+
+  KDTree(const KDTree&)            = delete;
+  KDTree& operator=(const KDTree&) = delete;
+  KDTree(KDTree&&)                 = delete;
+  KDTree& operator=(KDTree&&)      = delete;
+  ~KDTree();
+
+  void nearestNeighbours(
+      SKDCluster const& pt, int N, SharedKDClusters& result,
+      std::function<bool(SKDCluster const&)> const& filter = [](SKDCluster const&) { return false; });
+  void allNeighboursInRadius(
+      SKDCluster const& pt, const double radius, SharedKDClusters& result,
+      std::function<bool(SKDCluster const&)> const& filter = [](SKDCluster const&) { return false; });
+  void allNeighboursInTheta(
+      SKDCluster const& pt, const double thetaRange, SharedKDClusters& result,
+      std::function<bool(SKDCluster const&)> const& filter = [](SKDCluster const&) { return false; });
+  void allNeighboursInTheta(
+      double theta, const double thetaRange, SharedKDClusters& result,
+      std::function<bool(SKDCluster const&)> const& filter = [](SKDCluster const&) { return false; });
+
+private:
+  void transformResults(
+      KDTreeResultVector& vec, SharedKDClusters& result,
+      std::function<bool(SKDCluster const&)> const& filter = [](SKDCluster const&) { return false; });
+  void transformThetaResults(
+      KDTreeResultVector& vec, SharedKDClusters& result,
+      std::function<bool(SKDCluster const&)> const& filter = [](SKDCluster const&) { return false; });
+
+  static const int              k;
+  boost::multi_array<double, 2> array{};
+  boost::multi_array<double, 2> arrayTheta{};
+  kdtree2::KDTree*              tree      = nullptr;
+  kdtree2::KDTree*              treeTheta = nullptr;
+  SharedKDClusters              det{};
+  std::map<double, SKDCluster>  thetaLookup{};
+  bool                          sortTreeResults = true;
+};
+
+bool distComparator(const kdtree2::KDTreeResult& a, const kdtree2::KDTreeResult& b);
+
+using UKDTree = std::unique_ptr<KDTree>;
+
+#endif
