964285931a4abc9086ea5d1ff62f3f0bbb6d5253
jmcarcell <jmcarcell@users.noreply.github.com>
2025-07-17 18:38:14 +0200

    Use m_ with the members

--- k4Reco/GaudiLumiCalClusterer/src/LCCluster.cpp
+++ k4Reco/GaudiLumiCalClusterer/src/LCCluster.cpp
@@ -25,34 +25,34 @@
 
 LCCluster::LCCluster() {}
 
-LCCluster::LCCluster(const VirtualCluster& vc) : _position{vc.getX(), vc.getY(), vc.getZ()} {}
+LCCluster::LCCluster(const VirtualCluster& vc) : m_position{vc.getX(), vc.getY(), vc.getZ()} {}
 
 LCCluster::LCCluster(double energy, double x, double y, double z, double weight,
                      GlobalMethodsClass::WeightingMethod_t method, double theta, double phi,
                      VecCalHit const& caloHitVector)
-    : _position{x, y, z}, _energy(energy), _weight(weight), _method(method), _theta(theta), _phi(phi),
-      _caloHits(caloHitVector) {
+    : m_position{x, y, z}, m_energy(energy), m_weight(weight), m_method(method), m_theta(theta), m_phi(phi),
+      m_caloHits(caloHitVector) {
   CalculatePhi();
 }
 
 void LCCluster::clear() {
-  _energy = 0.0;
-  _position[0] = 0.0;
-  _position[1] = 0.0;
-  _position[2] = 0.0;
-  _weight = 0.0;
-  _method = GlobalMethodsClass::LogMethod;
-  _theta = 0.0;
-  _phi = 0.0;
-  _caloHits.clear();
+  m_energy = 0.0;
+  m_position[0] = 0.0;
+  m_position[1] = 0.0;
+  m_position[2] = 0.0;
+  m_weight = 0.0;
+  m_method = GlobalMethodsClass::LogMethod;
+  m_theta = 0.0;
+  m_phi = 0.0;
+  m_caloHits.clear();
 }
 
 std::ostream& operator<<(std::ostream& o, const LCCluster& rhs) {
-  o << "  Energy " << std::setw(10) << rhs._energy << "  Method " << std::setw(4) << rhs._method << "  Weight "
-    << std::setw(10) << rhs._weight << "  N Calo Hits " << std::setw(7) << rhs._caloHits.size() << "  pos(x,y,z) =  ( "
-    << std::setw(10) << rhs._position[0] << " , " << std::setw(10) << rhs._position[1] << " , " << std::setw(10)
-    << rhs._position[2] << " )"
-    << "  pos(theta,phi) =  ( " << std::setw(10) << rhs._theta << " , " << std::setw(10) << rhs._phi << " )";
+  o << "  Energy " << std::setw(10) << rhs.m_energy << "  Method " << std::setw(4) << rhs.m_method << "  Weight "
+    << std::setw(10) << rhs.m_weight << "  N Calo Hits " << std::setw(7) << rhs.m_caloHits.size() << "  pos(x,y,z) =  ( "
+    << std::setw(10) << rhs.m_position[0] << " , " << std::setw(10) << rhs.m_position[1] << " , " << std::setw(10)
+    << rhs.m_position[2] << " )"
+    << "  pos(theta,phi) =  ( " << std::setw(10) << rhs.m_theta << " , " << std::setw(10) << rhs.m_phi << " )";
   return o;
 }
 
@@ -64,20 +64,20 @@ void LCCluster::recalculatePositionFromHits(GlobalMethodsClass const& gmc) {
   const double logConstant(gmc.m_globalParamD.at(GlobalMethodsClass::LogWeightConstant));
 
   // re-set new cluster energy
-  _energy = 0.0;
-  for (auto const& calHit : _caloHits) {
-    _energy += calHit->getEnergy();
+  m_energy = 0.0;
+  for (auto const& calHit : m_caloHits) {
+    m_energy += calHit->getEnergy();
   }
 
   const double rMin = gmc.m_globalParamD.at(GlobalMethodsClass::RMin);
 
   double thetaTemp(0.0), weightsTemp(0.0), xTemp(0.0), yTemp(0.0), zTemp(0.0);
-  for (auto const& calHit : _caloHits) {
+  for (auto const& calHit : m_caloHits) {
     const auto pos = calHit->getPosition();
     const double rCell = sqrt(pos[0] * pos[0] + pos[1] * pos[1]);
     // cell area scales with radius, reduce weight for cells at larger radii
     const double weight =
-        GlobalMethodsClass::posWeight(calHit->getEnergy(), _energy, _method, logConstant) * rMin / rCell;
+        GlobalMethodsClass::posWeight(calHit->getEnergy(), m_energy, m_method, logConstant) * rMin / rCell;
 
     if (not(weight > 0))
       continue;
@@ -99,23 +99,23 @@ void LCCluster::recalculatePositionFromHits(GlobalMethodsClass const& gmc) {
 
   thetaTemp /= weightsTemp;
 
-  _phi = atan2(yTemp, xTemp);
-  _theta = thetaTemp;
+  m_phi = atan2(yTemp, xTemp);
+  m_theta = thetaTemp;
 
-  if (_theta < 0)
-    _theta += M_PI;
+  if (m_theta < 0)
+    m_theta += M_PI;
   const double sign = zTemp < 0 ? -1.0 : 1.0;
 
   const double r = sqrt(xTemp * xTemp + yTemp * yTemp + zTemp * zTemp);
   const double zStart = sign * gmc.m_globalParamD.at(GlobalMethodsClass::ZStart);
 
-  _position[0] = r * sin(_theta) * cos(_phi);
-  _position[1] = r * sin(_theta) * sin(_phi);
-  _position[2] = r * cos(_theta);
+  m_position[0] = r * sin(m_theta) * cos(m_phi);
+  m_position[1] = r * sin(m_theta) * sin(m_phi);
+  m_position[2] = r * cos(m_theta);
 
   // cluster position at front-face of LumiCal
-  _rzStart = tan(_theta) * zStart;
-  _positionAtFront[0] = _rzStart * cos(_phi);
-  _positionAtFront[1] = _rzStart * sin(_phi);
-  _positionAtFront[2] = zStart;
+  m_rzStart = tan(m_theta) * zStart;
+  m_positionAtFront[0] = m_rzStart * cos(m_phi);
+  m_positionAtFront[1] = m_rzStart * sin(m_phi);
+  m_positionAtFront[2] = zStart;
 }
