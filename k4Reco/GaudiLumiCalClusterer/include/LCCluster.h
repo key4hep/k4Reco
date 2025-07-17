5d5a4805170bc2e35dd53b4b0c0d3f68aae23239
jmcarcell <jmcarcell@users.noreply.github.com>
2025-07-17 18:38:14 +0200

    Use m_ with the members

--- k4Reco/GaudiLumiCalClusterer/include/LCCluster.h
+++ k4Reco/GaudiLumiCalClusterer/include/LCCluster.h
@@ -16,8 +16,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#ifndef K4RECO_LCClUSTER_H
-#define K4RECO_LCClUSTER_H 1
+#ifndef K4RECO_LCCLUSTER_H
+#define K4RECO_LCCLUSTER_H 1
 
 #include "Global.h"
 #include "GlobalMethodsClass.h"
@@ -25,6 +25,7 @@
 #include <cmath>
 #include <memory>
 #include <ostream>
+#include <array>
 
 class VirtualCluster;
 
@@ -42,48 +43,48 @@ public:
 
   void clear();
 
-  inline double getX() const { return _position[0]; }
-  inline double getY() const { return _position[1]; }
-  inline double getZ() const { return _position[2]; }
-  inline double getE() const { return _energy; }
-  inline double getWeight() const { return _weight; }
-  inline GlobalMethodsClass::WeightingMethod_t getMethod() const { return _method; }
+  inline double getX() const { return m_position[0]; }
+  inline double getY() const { return m_position[1]; }
+  inline double getZ() const { return m_position[2]; }
+  inline double getE() const { return m_energy; }
+  inline double getWeight() const { return m_weight; }
+  inline GlobalMethodsClass::WeightingMethod_t getMethod() const { return m_method; }
   inline double getEnergy() const { return getE(); }
-  inline double getTheta() const { return _theta; }
-  inline double getPhi() const { return _phi; }
-  inline double getRZStart() const { return _rzStart; }
+  inline double getTheta() const { return m_theta; }
+  inline double getPhi() const { return m_phi; }
+  inline double getRZStart() const { return m_rzStart; }
 
-  inline const double* getPosition() const { return _position; }
-  inline const double* getPositionAtFront() const { return _positionAtFront; }
+  inline const std::array<double, 3>& getPosition() const { return m_position; }
+  inline const std::array<double, 3>& getPositionAtFront() const { return m_positionAtFront; }
 
   inline void setX(double x) {
-    _position[0] = x;
+    m_position[0] = x;
     CalculatePhi();
   }
   inline void setY(double y) {
-    _position[1] = y;
+    m_position[1] = y;
     CalculatePhi();
   }
   inline void setZ(double z) {
-    _position[2] = z;
+    m_position[2] = z;
     CalculateTheta();
   }
   inline void setPosition(double x, double y, double z) {
-    _position[0] = x;
-    _position[1] = y;
-    _position[2] = z;
+    m_position[0] = x;
+    m_position[1] = y;
+    m_position[2] = z;
     CalculatePhi();
     CalculateTheta();
   }
-  inline void setWeight(double w) { _weight = w; }
-  inline void setTheta(double t) { _theta = t; }
-  inline void setPhi(double p) { _phi = p; }
+  inline void setWeight(double w) { m_weight = w; }
+  inline void setTheta(double t) { m_theta = t; }
+  inline void setPhi(double p) { m_phi = p; }
 
-  void addToEnergy(double E) { _energy += E; }
+  void addToEnergy(double E) { m_energy += E; }
 
   friend std::ostream& operator<<(std::ostream& o, const LCCluster& rhs);
 
-  inline VecCalHit const& getCaloHits() const { return _caloHits; }
+  inline VecCalHit const& getCaloHits() const { return m_caloHits; }
 
   /// calculate the cluster position based on the caloHits associated to the cluster
   void recalculatePositionFromHits(GlobalMethodsClass const& gmc);
@@ -91,23 +92,23 @@ public:
 private:
   void CalculatePhi();
   void CalculateTheta();
-  double _position[3] = {0.0, 0.0, 0.0};
-  double _positionAtFront[3] = {0.0, 0.0, 0.0};
-  double _energy = 0.0, _weight = 0.0;
-  GlobalMethodsClass::WeightingMethod_t _method = GlobalMethodsClass::LogMethod;
-  double _theta = 0.0, _phi = 0.0, _rzStart = 0.0;
-  VecCalHit _caloHits{};
+  std::array<double, 3> m_position{0.0, 0.0, 0.0};
+  std::array<double, 3> m_positionAtFront{0.0, 0.0, 0.0};
+  double m_energy = 0.0, m_weight = 0.0;
+  GlobalMethodsClass::WeightingMethod_t m_method = GlobalMethodsClass::LogMethod;
+  double m_theta = 0.0, m_phi = 0.0, m_rzStart = 0.0;
+  VecCalHit m_caloHits{};
 };
 
 inline void LCCluster::CalculatePhi() {
-  _phi = atan2(_position[1], _position[0]);
+  m_phi = atan2(m_position[1], m_position[0]);
   return;
 }
 inline void LCCluster::CalculateTheta() {
-  _theta = atan(sqrt(_position[1] * _position[1] + _position[0] * _position[0]) / fabs(_position[2]));
+  m_theta = atan(sqrt(m_position[1] * m_position[1] + m_position[0] * m_position[0]) / fabs(m_position[2]));
   return;
 }
 
 using SLCCluster = std::shared_ptr<LCCluster>;
 
-#endif // K4RECO_LCClUSTER_H
+#endif // K4RECO_LCCLUSTER_H
